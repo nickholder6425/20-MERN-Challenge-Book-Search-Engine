4d4e22e4b596bc5ee2fe7fe2a14c831788baf543
nicholasholder <ngholder@hotmail.com>
2024-04-07 12:17:51 -0500

    modified files

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,29 +1,17 @@
-import gql from "graphql-tag";
+import { gql } from "@apollo/client";
 
-// mutation for logged in user
 export const LOGIN_USER = gql`
   mutation loginUser($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
+    loginUser(email: $email, password: $password) {
       token
       user {
         _id
         username
-        email
-        bookCount
-        savedBooks {
-          bookId
-          title
-          description
-          authors
-          link
-          image
-        }
       }
     }
   }
 `;
 
-// mutation to add user
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
@@ -31,56 +19,39 @@ export const ADD_USER = gql`
       user {
         _id
         username
-        email
-        bookCount
-        savedBooks {
-          bookId
-          title
-          description
-          authors
-          image
-          link
-        }
       }
     }
   }
 `;
 
-// mutation to save books
 export const SAVE_BOOK = gql`
-  mutation SaveBook($book: BookInput) {
-    saveBook(book: $book) {
+  mutation saveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
       _id
       username
       email
       savedBooks {
         bookId
         authors
-        image
         description
         title
+        image
         link
       }
     }
   }
+
 `;
 
-// mutation to remove books
 export const REMOVE_BOOK = gql`
-  mutation removeBook($bookId: String!) {
-    removeBook(bookId: $bookId) {
-      _id
-      username
-      email
-      bookCount
-      savedBooks {
-        bookId
-        authors
-        image
-        description
-        title
-        link
-      }
+  mutation removeBook($book: BookInput!) {
+    removeBook(book: $book) {
+      bookId
+      authors
+      description
+      title
+      image
+      link
     }
   }
 `;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,20 +1,19 @@
-import gql from "graphql-tag";
+import { gql } from "@apollo/client";
 
-// queries for logged in users
 export const GET_ME = gql`
-  query {
+  {
     me {
       _id
       username
       email
       bookCount
       savedBooks {
-        bookId
-        authors
-        image
-        description
-        title
-        link
+          bookId
+          authors
+          description
+          title
+          image
+          link
       }
     }
   }
